6907ddb4b772b6d9796be325ff68c114a57a800a
realbro <realbro.noh@gmail.com>
2023-12-23 22:43:49 +0900

    GridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -21,7 +22,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: // Todo ...,
+      home: const CategoriesScreen(),
     );
   }
 }
